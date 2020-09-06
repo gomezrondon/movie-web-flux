01436601530c13f6b7e0e8e1c6578eea192ccc10
gomezrondon <gomezrondon@gmail.com>
2020-09-05 20:07:41 -0600

    Adding include-stacktrace flag

--- src/main/java/com/gomezrondon/moviewebflux/controller/MovieController.java
+++ src/main/java/com/gomezrondon/moviewebflux/controller/MovieController.java
@@ -3,11 +3,13 @@ package com.gomezrondon.moviewebflux.controller;
 
 import com.gomezrondon.moviewebflux.entity.Movie;
 import com.gomezrondon.moviewebflux.service.MovieService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -23,13 +25,12 @@ public class MovieController {
 
     @GetMapping("/list")
     public Flux<Movie> getAllMovies() {
-        //  Movie movie = service.getAllMovies().blockFirst();
-
         return service.getAllMovies();
     }
 
     @GetMapping("/{id}")
     public Mono<Movie> getMovieByID(@PathVariable int id) {
-        return service.findById(id);
+        return service.findById(id)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Movie not found")));
     }
 }
