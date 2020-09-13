436ddcdc710d434f493d47256b870d8a4eb0137b
gomezrondon <gomezrondon@gmail.com>
2020-09-13 15:09:34 -0600

    Adding 4 endpoint test cases.

--- src/test/java/com/gomezrondon/moviewebflux/MovieControllerTest.java
+++ src/test/java/com/gomezrondon/moviewebflux/MovieControllerTest.java
@@ -3,6 +3,7 @@ package com.gomezrondon.moviewebflux;
 
 import com.gomezrondon.moviewebflux.entity.Movie;
 import com.gomezrondon.moviewebflux.service.MovieService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +16,9 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,8 +34,67 @@ public class MovieControllerTest  {
     @Autowired
     private WebTestClient webTestClient;
 
+
     private final String baseUrl = "http://localhost:8080/movie";
 
+
+
+    @Test
+    @DisplayName("test endpoint /movie/{id} Not Found")
+    public void getByID2() {
+        webTestClient.get().uri(baseUrl + "/{id}", 100)
+                .exchange()
+                .expectStatus().isNotFound();
+
+    }
+
+    @Test
+    @DisplayName("test endpoint /movie/{id} ")
+    public void getByID() {
+        Flux<Movie> flux = webTestClient.get().uri(baseUrl + "/{id}", 1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_STREAM_JSON_VALUE)
+                .returnResult(Movie.class)
+                .getResponseBody();
+
+        StepVerifier.create(flux)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+
+    @Test
+    @DisplayName("test endpoint /movie/list v3")
+    public void getAllItemsV3() {
+        Flux<Movie> flux = webTestClient.get().uri(baseUrl + "/list")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_STREAM_JSON_VALUE)
+                .returnResult(Movie.class)
+                .getResponseBody();
+
+        StepVerifier.create(flux)
+                .expectNextCount(9)
+                .verifyComplete();
+    }
+
+
+    @Test
+    @DisplayName("test endpoint /movie/list v2")
+    public void getAllItemsV2() {
+        webTestClient.get().uri(baseUrl + "/list")
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_STREAM_JSON_VALUE)
+                .expectBodyList(Movie.class)
+                .consumeWith(response -> {
+                    List<Movie> movieList = response.getResponseBody();
+                    movieList.forEach(movie -> {
+                        Assertions.assertTrue(movie.getId() != null);
+                    });
+                });
+    }
 
     @Test
     @DisplayName("test endpoint /movie/list")
