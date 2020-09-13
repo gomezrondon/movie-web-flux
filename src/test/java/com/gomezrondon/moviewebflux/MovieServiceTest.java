c58f15ec857f3bb35ed91cba3b26ee6919c484b3
gomezrondon <gomezrondon@gmail.com>
2020-09-12 18:57:35 -0600

    Setting dev profile for testing.

--- src/test/java/com/gomezrondon/moviewebflux/MovieServiceTest.java
+++ src/test/java/com/gomezrondon/moviewebflux/MovieServiceTest.java
@@ -7,11 +7,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import reactor.test.StepVerifier;
 
-@SpringBootTest(classes = Application.class)
-//@ExtendWith(SpringExtension.class)
+@SpringBootTest
 @Slf4j
+@ActiveProfiles("dev")
 public class MovieServiceTest {
 
     @Autowired
