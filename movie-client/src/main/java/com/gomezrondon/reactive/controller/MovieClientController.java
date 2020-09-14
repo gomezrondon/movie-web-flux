429b926cfe9b2a31131cc6d5697209e8bb5a05ba
gomezrondon <gomezrondon@gmail.com>
2020-09-14 11:42:50 -0600

    Creating first endpoint for movie-client

--- movie-client/src/main/java/com/gomezrondon/reactive/controller/MovieClientController.java
+++ movie-client/src/main/java/com/gomezrondon/reactive/controller/MovieClientController.java
@@ -1,7 +1,21 @@
 package com.gomezrondon.reactive.controller;
 
+import com.gomezrondon.reactive.entity.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class MovieClientController {
+
+    WebClient webClient = WebClient.create("http://localhost:8080");
+
+    @GetMapping("/client/retrieve")
+    public Flux<Movie> getAllMovies() {
+        return webClient.get().uri("/movie/list")
+                .retrieve()
+                .bodyToFlux(Movie.class)
+                .log("Movies in Client project:");
+    }
 }
